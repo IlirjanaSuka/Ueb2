10cad21154f668fc49702d40dd859fdedc18271f
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2023-05-04 22:58:21 +0200

    Add files via upload

---
+++ Ueb2/menu.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="menu.css">
+    </head>
+<body>
+<section class="main">
+            <nav>
+                <a href="#" class="logo">
+                    <h1>FindYourWay</h1>
+                </a>
+                <ul class="menu">
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">Drejtimet</a></li>
+                    <li><a href="#">LogIn</a></li>
+                    <li><a href="#">SignUp</a></li>
+                </ul>
+            </nav>
+</section>
+</body>
+</html>
