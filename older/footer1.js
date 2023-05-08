6d8ec9d17cd964e07d85c2ab11f685405ad7be43
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-08 02:25:05 +0200

    Create footer1.js

---
+++ older/footer1.js
@@ -0,0 +1,22 @@
+function message(){
+    var email = document.getElementById('email');
+    var msg = document.getElementById('msg');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+    if (email.value === '' || msg.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() =>{
+            email.value = '';
+            msg.value = '';
+        },2000);
+        success.style.display = 'block';
+    }
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    
+     }, 4000 );
+}
