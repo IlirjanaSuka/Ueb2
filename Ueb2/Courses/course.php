18ae621604bfa186fc517cab058a5cae61fa1dec
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-10 04:37:27 +1400

    Update course.php

--- Ueb2/Courses/course.php
+++ Ueb2/Courses/course.php
@@ -7,47 +7,58 @@
     <title>Document</title>
 </head>
 <body>
- <section class="teachers">
+<section class="teachers">
     <h1 class="heading">Expert Tutors</h1>
     <div class="box-container">
         <?php
-        $select_tutors=$conn->prepare("SELECT*FROM 'tutors'");
-        $select_tutors->execute();
-        if($select_tutors->rowCount()>0){
-while($fetch_tutor=$select_tutors->fetch(PDO::FETCH_ASSOC)){
-    $tutor_id=$fetch_tutor['id'];
+        // Establish a database connection
+        $servername = "your_servername";
+        $username = "your_username";
+        $password = "your_password";
+        $dbname = "your_database_name";
 
-    $count_student=$conn->prepare("SELECT*FROM 'student' WHERE tutor_id=?");
-    $count_student->execute([$tutor_id]);
-    $total_student=$count_student->rowCount();
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $count_Assignments=$conn->prepare("SELECT * FROM 'Assignments' $count_Assignments' WHERE tutor_id=?");
-    $count_Assignments->execute([$tutor_id]);
-    $total_Assignments=$count_Assignments->rowCount();
-}
-        
-?>
-<div class="box">
-    <div class="tutor">
-        <img src="uploaded_files/<?=$fetch_tutor['image'];?>" alt="">
-        <div>
-            <h3>
-                <?=$fetch_tutor['name'];?></h3>
-                <span><?=$fetch_tutor['profession'];?></span>
-            </h3>
-        </div>
+            $select_tutors = $conn->prepare("SELECT * FROM tutors");
+            $select_tutors->execute();
 
-    </div>
-    <p>Total students:</p><span><?=$total_student;?></span>
-    <p>Total Assignments:</p><span><?=$total_Assignments;?></span>
-    <a href="tutor_profile.php?get_id=<? $fetch_tutor['email'];?> " class="inline-btn">View Profile</a>
-</div>
-<?php
-        }else{
-            echo '';
+            if ($select_tutors->rowCount() > 0) {
+                while ($fetch_tutor = $select_tutors->fetch(PDO::FETCH_ASSOC)) {
+                    $tutor_id = $fetch_tutor['id'];
+
+                    $count_student = $conn->prepare("SELECT * FROM student WHERE tutor_id=?");
+                    $count_student->execute([$tutor_id]);
+                    $total_student = $count_student->rowCount();
+
+                    $count_Assignments = $conn->prepare("SELECT * FROM Assignments WHERE tutor_id=?");
+                    $count_Assignments->execute([$tutor_id]);
+                    $total_Assignments = $count_Assignments->rowCount();
+                    ?>
+                    <div class="box">
+                        <div class="tutor">
+                            <img src="uploaded_files/<?= $fetch_tutor['image']; ?>" alt="">
+                            <div>
+                                <h3><?= $fetch_tutor['name']; ?></h3>
+                                <span><?= $fetch_tutor['profession']; ?></span>
+                            </div>
+                        </div>
+                        <p>Total students:</p><span><?= $total_student; ?></span>
+                        <p>Total Assignments:</p><span><?= $total_Assignments; ?></span>
+                        <a href="tutor_profile.php?get_id=<?= $fetch_tutor['email']; ?>" class="inline-btn">View Profile</a>
+                    </div>
+                <?php
+                }
+            } else {
+                echo '';
+            }
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
-        ?><p class="empty">no tutors was not found!</p>
+        ?>
+        <p class="empty">No tutors were found!</p>
     </div>
- </section>   
+</section>
 </body>
 </html>
