48b562897e035b9c39cfa05cb923e431f2ec4e02
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-16 04:32:38 +1400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    Let slides=document.querySelectorAll('testimonial-item');
+    Let index=0;
+    function nextSlide(){
+        slides[index].classList.remove('active');
+        index=(index+1)%slides.length;
+        slides[index].classList.add('active');
+    }
+    function prevSlide(){
+        slides[index].classList.remove('active');
+        index=(index-1+slides.length)%slides.length;
+        slides[index].classList.add('active');
+    }
