f0645bbb1cd13c1f27a680dde6ce4c8a4f7b7896
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-05-16 04:28:52 +1400

    Delete Ueb2 directory

--- Ueb2/script.js
+++
@@ -1,8 +0,0 @@
-
-  var currentUrl = window.location.href;
-  var links = document.querySelectorAll('.menu li a');
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].href === currentUrl) {
-      links[i].classList.add('active');
-    }
-  }
