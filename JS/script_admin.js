10fe491b5e36e4d9da671a2cd2c1a524d6b0b3ac
Liburna Berisha <115898216+Liburna1@users.noreply.github.com>
2023-05-21 01:45:44 +0200

    Add files via upload

---
+++ JS/script_admin.js
@@ -0,0 +1,6 @@
+//close form
+ const closeBtn = document.querySelector('#close-edit');
+
+ closeBtn.addEventListener('click', ()=>{
+    document.querySelector('.update-container').getElementsByClassName.display='none';
+ })
