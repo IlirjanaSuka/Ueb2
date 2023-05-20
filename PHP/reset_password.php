ed42b0c8d86ac755c644e0818d80ec95bb7e9020
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-20 21:43:34 +0200

    Update reset_password.php

--- PHP/reset_password.php
+++ PHP/reset_password.php
@@ -1,40 +1,27 @@
 <?php
-include 'connection.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
 
-if(isset($_SESSION['user_id'])){
-    $user_id = $_SESSION['user_id'];
-} else {
-    $user_id = '';
+
+    $resetCode = generateRandomCode(); // Krijoni funksionin tuaj për të gjeneruar një kod të rastësishëm
+
+    $subject = "Rivendosja e fjalëkalimit";
+    $message = "Për të rivendosur fjalëkalimin tuaj, shkruani këtë kod: " . $resetCode;
+    $headers = "From: webmaster@example.com";
+    mail($email, $subject, $message, $headers);
+
+    header("Location: reset_password_form.php");
+    exit();
 }
 
-if(isset($_GET['token'])){
-    $token = $_GET['token'];
+function generateRandomCode() {
+    $length = 8;
+    $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $randomCode = "";
 
-    // Check if the token exists in the database and has not expired
-    $query = $conn->prepare("SELECT * FROM password_resets WHERE token = ? AND created_at >= DATE_SUB(NOW(), INTERVAL 1 HOUR)");
-    $query->execute([$token]);
-    $row = $query->fetch(PDO::FETCH_ASSOC);
-
-    if($row){
-        // Token is valid, allow the user to reset the password
-        if(isset($_POST['submit'])){
-            $newPassword = $_POST['new_password'];
-            // Update the user's password in the database
-            // ...
-            echo "Password has been reset successfully!";
-        }
-    } else {
-        echo "Invalid or expired token!";
+    for ($i = 0; $i < $length; $i++) {
+        $randomCode .= $characters[rand(0,     strlen($characters) - 1)];
     }
-} else {
-    echo "Token not found!";
-}
 
-// Rest of the code remains the same
-?>
-<link rel="stylesheet" href="style.css">
-<!-- HTML form -->
-<form action="" method="post" class="form-container">
-    <input type="password" name="new_password" placeholder="Enter new password" required>
-    <input type="submit" name="submit" value="Reset Password">
-</form>
+    return $randomCode;
+}
