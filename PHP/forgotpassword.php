7a39b9c2c2b9e43d3fd84e985247bbb4a7f826a7
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-20 21:44:07 +0200

    Create forgotpassword.php

---
+++ PHP/forgotpassword.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="reset_password.php">
+    <input type="email" name="email" placeholder="Emaili juaj">
+    <input type="submit" value="Rivendos fjalëkalimin">
+</form>
+</body>
+</html>
