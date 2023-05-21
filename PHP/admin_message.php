84ddbd1c61420f93235e419209eb36732be1f8d0
Ilirjana Suka <118145049+IlirjanaSuka@users.noreply.github.com>
2023-05-21 19:09:38 +0200

    Update admin_message.php

--- PHP/admin_message.php
+++ PHP/admin_message.php
@@ -1,23 +1,23 @@
-<!--<?php
+<?php
 session_start();
 $admin_id = $_SESSION['admin_id'];
-if(!isset($admin_id)){
-    header('loaction:login.php');
+if (!isset($admin_id)) {
+    header('Location: login.php');
+    exit();
 }
-if (isset($_POST['logout'])){
+if (isset($_POST['logout'])) {
     session_destroy();
-    header('loaction:login.php');
+    header('Location: login.php');
+    exit();
 }
-/*deletin orders */
-if(isset($_GET['delete'])){
+/* Deleting orders */
+if (isset($_GET['delete'])) {
     $delete_id = $_GET['delete'];
-   
-    mysqli_query($conn, "DELETE FROM 'message' WHERE id=$delete_id") or die('query failed');
-
-    header('loaction:admin_user.php');
+    mysqli_query($conn, "DELETE FROM `message` WHERE id=$delete_id") or die('Query failed');
+    header('Location: admin_user.php');
+    exit();
 }
-
-?>-->
+?>
 
 <!DOCTYPE html>
 <html>
@@ -30,39 +30,37 @@ if(isset($_GET['delete'])){
     <body>
         <?php include 'admin_header.php'?>
         <?php
-         if(isset($message)){
-             foreach ($message as $message){
-                 echo '
-                 <div class="message">
-                 <span>'.$message.'</span>
-                 <i class="bi bi-x-circle onclick="this.parentElement.remove()"></i>
-                 </div>';
-             }
-         }
-         ?>
+        if (isset($message)) {
+            foreach ($message as $msg) {
+                echo '
+                <div class="message">
+                    <span>'.$msg.'</span>
+                    <i class="bi bi-x-circle onclick="this.parentElement.remove()"></i>
+                </div>';
+            }
+        }
+        ?>
         <section class="order-container">
             <h1 class="title">message</h1>
             <div class="box-container">
             <?php
-                $select_message = mysqli_query($conn, "SELECT * FROM 'message' ") or die('query failed');
-                if(mysqli_num_rows($select_message)>0){
-                    while($fetch_message  = mysqli_fetch_assoc($select_message )){
-
-                ?>
-                <div class="box">
-                <p>user id: <span><?php echo $fetch_message ['user_id'] ;?></span></p>
-                <p>user name: <span><?php echo $fetch_message ['name'] ;?></span></p>
-                <p>email: <span><?php echo $fetch_message ['email'] ;?></span></p>
-                <p><?php echo $fetch_message ['message'] ;?></p>
-                <a href="admin_message.php?delete=<?php echo $fetch_message ['id']?>"class="delete"
-                        onclick="return confirm('delete this')">Delete</a>
-                </div>
-                <?php
-                        }
-                    }else{
-                        echo '<p class="empty">no message yet</p>';
+                $select_message = mysqli_query($conn, "SELECT * FROM `message`") or die('Query failed');
+                if (mysqli_num_rows($select_message) > 0) {
+                    while ($fetch_message = mysqli_fetch_assoc($select_message)) {
+                        ?>
+                        <div class="box">
+                            <p>user id: <span><?php echo $fetch_message['user_id'];?></span></p>
+                            <p>user name: <span><?php echo $fetch_message['name'];?></span></p>
+                            <p>email: <span><?php echo $fetch_message['email'];?></span></p>
+                            <p><?php echo $fetch_message['message'];?></p>
+                            <a href="admin_message.php?delete=<?php echo $fetch_message['id'];?>" class="delete" onclick="return confirm('delete this')">Delete</a>
+                        </div>
+                        <?php
                     }
-                ?>
+                } else {
+                    echo '<p class="empty">no message yet</p>';
+                }
+            ?>
             </div>
         </section>
         <script src="script_admin.js"></script>
